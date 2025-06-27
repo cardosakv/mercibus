9392de3d5397fd3d8a764c83dde2131e9e0fe9ca
cardosakv <kvcards26@gmail.com>
2025-06-27 14:22:55 +0800

    Add Messages class for user feedback constants

---
+++ services/auth/AuthService/AuthService.Application/Common/Messages.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auth.Application.Common
+{
+    public static class Messages
+    {
+        public const string USER_REGISTERED = "User registered successfully.";
+        public const string UNEXPECTED_ERROR = "An unexpected error occurred while processing your request.";
+    }
+}
