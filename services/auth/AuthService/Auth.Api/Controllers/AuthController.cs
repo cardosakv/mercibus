728555405e1294aad3095231271976f085b1aa54
cardosakv <kvcards26@gmail.com>
2025-06-29 02:07:20 +0800

    Simplify controller route annotations

--- services/auth/AuthService/Auth.Api/Controllers/AuthController.cs
+++ services/auth/AuthService/Auth.Api/Controllers/AuthController.cs
@@ -10,32 +10,28 @@ namespace Auth.Api.Controllers
     [ApiController]
     public class AuthController(IAuthService authService) : BaseController
     {
-        [HttpPost]
-        [Route("register")]
+        [HttpPost("register")]
         public async Task<IActionResult> Register([FromBody] RegisterRequest request)
         {
             var response = await authService.RegisterAsync(request);
             return HandleResponse(response, HttpContext.Request.Method);
         }
 
-        [HttpPost]
-        [Route("login")]
+        [HttpPost("login")]
         public async Task<IActionResult> Login([FromBody] LoginRequest request)
         {
             var response = await authService.LoginAsync(request);
             return HandleResponse(response, HttpContext.Request.Method);
         }
 
-        [HttpPost]
-        [Route("refresh-token")]
+        [HttpPost("refresh-token")]
         public async Task<IActionResult> RefreshToken([FromBody] RefreshRequest request)
         {
             var response = await authService.RefreshTokenAsync(request);
             return HandleResponse(response, HttpContext.Request.Method);
         }
 
-        [HttpPost]
-        [Route("send-confirmation-email")]
+        [HttpPost("send-confirmation-email")]
         [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
         public async Task<IActionResult> SendConfirmationEmail([FromBody] SendConfirmationEmailRequest request)
         {
@@ -43,8 +39,7 @@ namespace Auth.Api.Controllers
             return HandleResponse(response, HttpContext.Request.Method);
         }
 
-        [HttpGet]
-        [Route("confirm-email")]
+        [HttpGet("confirm-email")]
         public async Task<IActionResult> ConfirmEmail(string userId, string token)
         {
             var response = await authService.ConfirmEmail(userId, token);
