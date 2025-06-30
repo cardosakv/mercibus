984cb7179c92e3e9e966c9e263fb13dce808caba
cardosakv <kvcards26@gmail.com>
2025-06-30 17:59:04 +0800

    Add auth service register tests

---
+++ services/auth/Auth.Tests/Application/AuthService/BaseTests.cs
@@ -0,0 +1,59 @@
+using Auth.Application.Interfaces;
+using Auth.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Auth.Tests.Application.AuthService;
+
+/// <summary>
+/// Base test class for auth service tests.
+/// </summary>
+public abstract class BaseTests
+{
+    protected readonly IAuthService AuthService;
+
+    protected readonly Mock<UserManager<User>> UserManagerMock;
+    protected readonly Mock<ITokenService> TokenServiceMock;
+    protected readonly Mock<IEmailService> EmailServiceMock;
+    protected readonly Mock<ITransactionService> TransactionServiceMock;
+    protected readonly Mock<IRefreshTokenRepository> RefreshTokenRepositoryMock;
+    protected readonly Mock<IHttpContextAccessor> HttpContextAccessorMock;
+    protected readonly Mock<LinkGenerator> LinkGeneratorMock;
+    protected readonly Mock<IConfiguration> ConfigurationMock;
+    protected readonly Mock<ILogger<IAuthService>> LoggerMock;
+
+    protected BaseTests()
+    {
+        var userStoreMock = new Mock<IUserStore<User>>();
+        UserManagerMock = new Mock<UserManager<User>>(userStoreMock.Object, null, null, null, null, null, null, null, null);
+
+        TransactionServiceMock = new Mock<ITransactionService>();
+        TransactionServiceMock.Setup(x => x.BeginAsync()).Returns(Task.CompletedTask);
+        TransactionServiceMock.Setup(x => x.CommitAsync()).Returns(Task.CompletedTask);
+        TransactionServiceMock.Setup(x => x.RollbackAsync()).Returns(Task.CompletedTask);
+
+        TokenServiceMock = new Mock<ITokenService>();
+
+        EmailServiceMock = new Mock<IEmailService>();
+        RefreshTokenRepositoryMock = new Mock<IRefreshTokenRepository>();
+        HttpContextAccessorMock = new Mock<IHttpContextAccessor>();
+        LinkGeneratorMock = new Mock<LinkGenerator>();
+        ConfigurationMock = new Mock<IConfiguration>();
+        LoggerMock = new Mock<ILogger<IAuthService>>();
+
+        AuthService = new Auth.Application.Services.AuthService(
+            UserManagerMock.Object,
+            TokenServiceMock.Object,
+            EmailServiceMock.Object,
+            TransactionServiceMock.Object,
+            RefreshTokenRepositoryMock.Object,
+            HttpContextAccessorMock.Object,
+            LinkGeneratorMock.Object,
+            ConfigurationMock.Object,
+            LoggerMock.Object);
+    }
+}
---
+++ services/auth/Auth.Tests/Application/AuthService/RegisterAsyncTests.cs
@@ -0,0 +1,105 @@
+using Auth.Application.DTOs;
+using Auth.Domain.Common;
+using Auth.Domain.Entities;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Auth.Tests.Application.AuthService;
+
+/// <summary>
+/// Tests for auth service register method.
+/// </summary>
+public class RegisterAsyncTests : BaseTests
+{
+    private readonly RegisterRequest _request = new()
+    {
+        Username = "test",
+        Password = "password",
+        Email = "test@email.com"
+    };
+
+    [Fact]
+    public async Task ReturnsSuccess_WhenUserCreatedAndRoleAdded()
+    {
+        // Arrange
+        UserManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+        UserManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        // Act
+        var response = await AuthService.RegisterAsync(_request);
+
+        // Assert
+        TransactionServiceMock.Verify(x => x.BeginAsync(), Times.Once);
+        UserManagerMock.Verify(x => x.CreateAsync(It.IsAny<User>(), _request.Password), Times.Once);
+        UserManagerMock.Verify(x => x.AddToRoleAsync(It.IsAny<User>(), Roles.Customer), Times.Once);
+        TransactionServiceMock.Verify(x => x.CommitAsync(), Times.Once);
+        response.IsSuccess.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task ReturnsFail_WhenUserCreatedAndNoRoleAdded()
+    {
+        // Arrange
+        UserManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+        UserManagerMock.Setup(x => x.AddToRoleAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Failed());
+
+        // Act
+        var response = await AuthService.RegisterAsync(_request);
+
+        // Assert
+        TransactionServiceMock.Verify(x => x.BeginAsync(), Times.Once);
+        UserManagerMock.Verify(x => x.CreateAsync(It.IsAny<User>(), _request.Password), Times.Once);
+        UserManagerMock.Verify(x => x.AddToRoleAsync(It.IsAny<User>(), Roles.Customer), Times.Once);
+        TransactionServiceMock.Verify(x => x.CommitAsync(), Times.Never);
+        response.IsSuccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task ReturnsFail_WhenUserNotCreated()
+    {
+        // Arrange
+        UserManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Failed());
+
+        // Act
+        var response = await AuthService.RegisterAsync(_request);
+
+        // Assert
+        TransactionServiceMock.Verify(x => x.BeginAsync(), Times.Once);
+        UserManagerMock.Verify(x => x.CreateAsync(It.IsAny<User>(), _request.Password), Times.Once);
+        UserManagerMock.Verify(x => x.AddToRoleAsync(It.IsAny<User>(), Roles.Customer), Times.Never);
+        TransactionServiceMock.Verify(x => x.CommitAsync(), Times.Never);
+        response.IsSuccess.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task ReturnsFail_WhenExceptionOccurs()
+    {
+        // Arrange
+        const string exceptionMessage = "Create user exception.";
+        UserManagerMock.Setup(x => x.CreateAsync(It.IsAny<User>(), It.IsAny<string>()))
+            .ThrowsAsync(new Exception(exceptionMessage));
+
+        // Act
+        var response = await AuthService.RegisterAsync(_request);
+
+        // Assert
+        TransactionServiceMock.Verify(x => x.BeginAsync(), Times.Once);
+        TransactionServiceMock.Verify(x => x.RollbackAsync(), Times.Once);
+        LoggerMock.Verify(
+            x => x.Log(
+                LogLevel.Error,
+                It.IsAny<EventId>(),
+                It.Is<It.IsAnyType>((v, t) => v.ToString()!.Contains(exceptionMessage)),
+                It.IsAny<Exception>(),
+                It.IsAny<Func<It.IsAnyType, Exception, string>>()!),
+            Times.Once);
+        response.IsSuccess.Should().BeFalse();
+    }
+}
