ece5000f6d8438316a52ce24bfa503cd6a1446a1
cardosakv <kvcards26@gmail.com>
2025-07-04 23:27:41 +0800

    Add catalog service initial migration

--- services/catalog/Catalog.Api/Program.cs
+++ services/catalog/Catalog.Api/Program.cs
@@ -1,8 +1,15 @@
+using Catalog.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 try
 {
     var builder = WebApplication.CreateBuilder(args);
 
     // Add services to the container.
+    
+    // Add database context.
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
     builder.Services.AddControllers();
     builder.Services.AddEndpointsApiExplorer();
--- services/catalog/Catalog.Infrastructure/AppDbContext.cs
+++ services/catalog/Catalog.Infrastructure/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Configurations;
 using Microsoft.EntityFrameworkCore;
 using Attribute = System.Attribute;
 
@@ -9,10 +10,22 @@ namespace Catalog.Infrastructure;
 /// </summary>
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
-    public DbSet<Attribute> Attributes { get; set; }
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Category> Categories { get; set; }
     public DbSet<Product> Products { get; set; }
+    public DbSet<ProductAttribute> Attributes { get; set; }
     public DbSet<ProductImage> ProductImages { get; set; }
     public DbSet<ProductReview> Reviews { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        
+        builder.ApplyConfiguration(new  BrandConfiguration());
+        builder.ApplyConfiguration(new  CategoryConfiguration());
+        builder.ApplyConfiguration(new ProductConfiguration());
+        builder.ApplyConfiguration(new ProductAttributeConfiguration());
+        builder.ApplyConfiguration(new ProductImageConfiguration());
+        builder.ApplyConfiguration(new ProductReviewConfiguration());
+    }
 }
