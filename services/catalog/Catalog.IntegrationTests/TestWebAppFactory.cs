44a10e14962288071d62dc3494e0409d4b9f7daa
cardosakv <kvcards26@gmail.com>
2025-07-29 21:16:41 +0800

    Ensure postgres container is started in integration tests

--- services/catalog/Catalog.IntegrationTests/TestWebAppFactory.cs
+++ services/catalog/Catalog.IntegrationTests/TestWebAppFactory.cs
@@ -1,4 +1,5 @@
 using Catalog.Infrastructure;
+using DotNet.Testcontainers.Builders;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,7 @@ public class TestWebAppFactory : WebApplicationFactory<Program>, IAsyncLifetime
         .WithImage("postgres:latest")
         .WithUsername("test_user")
         .WithPassword("test_password")
+        .WithWaitStrategy(Wait.ForUnixContainer().UntilPortIsAvailable(5432))
         .Build();
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
