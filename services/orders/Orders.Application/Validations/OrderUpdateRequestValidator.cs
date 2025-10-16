9d79b699588ec464578568cff4c7bffb625153e9
cardosakv <kvcards26@gmail.com>
2025-10-16 21:54:42 +0800

    Update order status request validation

--- services/orders/Orders.Application/Validations/OrderUpdateRequestValidator.cs
+++ services/orders/Orders.Application/Validations/OrderUpdateRequestValidator.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation;
 using Orders.Application.Common;
 using Orders.Application.DTOs;
+using Orders.Domain.Enums;
 
 namespace Orders.Application.Validations;
 
@@ -12,6 +13,7 @@ public class OrderUpdateRequestValidator : AbstractValidator<OrderUpdateRequest>
     public OrderUpdateRequestValidator()
     {
         RuleFor(x => x.Status)
-            .IsInEnum().WithMessage(Constants.ErrorCode.InvalidOrderStatus);
+            .Must(status => Enum.TryParse(typeof(OrderStatus), status, true, out _))
+            .WithMessage(Constants.ErrorCode.InvalidOrderStatus);
     }
 }
