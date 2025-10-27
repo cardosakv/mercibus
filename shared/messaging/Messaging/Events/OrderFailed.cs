ec43a57518ba6451fa2d0112eaa3454dbce51f7e
cardosakv <kvcards26@gmail.com>
2025-10-27 21:13:49 +0800

    Add OrderFailed event to represent failed orders

---
+++ shared/messaging/Messaging/Events/OrderFailed.cs
@@ -0,0 +1,13 @@
+﻿using Messaging.Models;
+
+namespace Messaging.Events;
+
+/// <summary>
+/// Event representing a failed order.
+/// </summary>
+public record OrderFailed(
+    long OrderId,
+    string CustomerId,
+    List<OrderItem> Items,
+    DateTime FailedAt
+);
