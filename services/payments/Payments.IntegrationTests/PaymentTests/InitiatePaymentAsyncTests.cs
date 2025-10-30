a49aee60b921acec842174718174f7382e4637c4
cardosakv <kvcards26@gmail.com>
2025-10-30 22:05:43 +0800

    Add test for invalid billing request in InitiatePaymentAsyncTests

--- services/payments/Payments.IntegrationTests/PaymentTests/InitiatePaymentAsyncTests.cs
+++ services/payments/Payments.IntegrationTests/PaymentTests/InitiatePaymentAsyncTests.cs
@@ -59,4 +59,51 @@ public class InitiatePaymentAsyncTests(WebAppFactory factory) : IClassFixture<We
         // Assert
         response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
+
+    [Fact]
+    public async Task ReturnsBadRequest_WhenBillingRequestIsInvalid()
+    {
+        // Arrange
+        var db = factory.CreateDbContext();
+        var payment = await db.Payments.AddAsync(
+            new Payment
+            {
+                OrderId = 15,
+                CustomerId = "cust-1",
+                Amount = 500,
+                Currency = "PHP",
+                Status = PaymentStatus.AwaitingUserAction
+            });
+        await db.SaveChangesAsync();
+
+        var billing = new BillingRequest(
+            null!, // invalid
+            null!, // invalid
+            "", // invalid
+            null,
+            "", // invalid
+            null,
+            "", // invalid
+            "", // invalid
+            0, // invalid
+            "" // invalid
+        );
+
+        var request = new PaymentRequest(payment.Entity.OrderId, billing);
+
+        var client = factory.CreateClient();
+        client.DefaultRequestHeaders.Add("Authorization", "Bearer test-token");
+
+        // Act
+        var response = await client.PostAsJsonAsync(Url, request);
+
+        // Assert
+        response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        var result = await response.Content.ReadFromJsonAsync<ApiErrorResponse>();
+        result.Should().NotBeNull();
+        result!.Error.Should().NotBeNull();
+        result.Error.Code.Should().Be("validation_failed");
+        result.Error.Params.Should().Contain(p => p.Field == "firstName");
+        result.Error.Params.Should().Contain(p => p.Field == "email");
+    }
 }
