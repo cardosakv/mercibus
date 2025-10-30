8de561f1472cec2f8ea8e81943672cee4723ef2d
cardosakv <kvcards26@gmail.com>
2025-10-30 22:28:53 +0800

    Add billing request validation to PaymentRequestValidator

--- services/payments/Payments.Application/Validations/PaymentRequestValidator.cs
+++ services/payments/Payments.Application/Validations/PaymentRequestValidator.cs
@@ -17,5 +17,8 @@ public class PaymentRequestValidator: AbstractValidator<PaymentRequest>
         RuleFor(x => x.OrderId)
             .MustAsync(async (id, cancellationToken) => await paymentRepository.GetPaymentByOrderIdAsync(id, cancellationToken) != null)
             .WithMessage(Constants.ErrorCode.OrderNotFound);
+
+        RuleFor(x => x.BillingRequest)
+            .SetValidator(new BillingRequestValidator());
     }
 }
