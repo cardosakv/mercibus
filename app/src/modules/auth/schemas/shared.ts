c062222f2464126928d50ca537b0594c184562be
cardosakv <kvcards26@gmail.com>
2025-11-15 22:46:50 +0800

    Refactor confirmPasswordSchema to simplify validation rules

--- app/src/modules/auth/schemas/shared.ts
+++ app/src/modules/auth/schemas/shared.ts
@@ -14,10 +14,4 @@ export const passwordSchema = z
   .regex(/^(?=.*\d)/, 'Password must contain at least one number.')
   .regex(/^(?=.*[\W_])/, 'Password must contain at least one special character.');
 
-export const confirmPasswordSchema = z
-  .string()
-  .min(8, 'Password must be at least 8 characters.')
-  .regex(/^(?=.*[A-Z])/, 'Password must contain at least one uppercase letter.')
-  .regex(/^(?=.*[a-z])/, 'Password must contain at least one lowercase letter.')
-  .regex(/^(?=.*\d)/, 'Password must contain at least one number.')
-  .regex(/^(?=.*[\W_])/, 'Password must contain at least one special character.');
+export const confirmPasswordSchema = z.string();
