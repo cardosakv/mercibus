30129d6a4f0ba656f2d4f91c95d86dd77150cd6f
cardosakv <kvcards26@gmail.com>
2025-11-15 18:46:51 +0800

    Add login page

--- app/src/routes/auth.tsx
+++ app/src/routes/auth.tsx
@@ -3,6 +3,7 @@ import { ROUTE_PATHS } from './paths';
 import type { RouteObject } from 'react-router-dom';
 import { EmailSuccessPage } from '@/modules/auth/pages/email-success-page';
 import { EmailErrorPage } from '@/modules/auth/pages/email-error-page';
+import { LoginPage } from '@/modules/auth/pages/login-page';
 
 export const authRoutes: RouteObject[] = [
   {
@@ -16,5 +17,9 @@ export const authRoutes: RouteObject[] = [
   {
     path: ROUTE_PATHS.EMAIL_FAILURE,
     element: <EmailErrorPage />,
+  },
+  {
+    path: ROUTE_PATHS.LOGIN,
+    element: <LoginPage />,
   },
 ];
--- app/src/routes/paths.ts
+++ app/src/routes/paths.ts
@@ -1,4 +1,5 @@
 export const ROUTE_PATHS = {
+  HOME: '/',
   SIGNUP: '/signup',
   EMAIL_SUCCESS: '/verification/email-success',
   EMAIL_FAILURE: '/verification/email-error',
